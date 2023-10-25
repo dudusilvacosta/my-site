482292bcc4761d1e3d97a0d5e84cc39772045f62
dudusilvacosta <duduroots83@gmail.com>
2023-10-25 18:48:54 -0300

    ajustes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+    var menu = document.getElementById("nav")
+    menu.style.right = 0
+}
+
+function fecharMenu() {
+    var menu = document.getElementById("nav")
+    menu.style.right = 500
+}
