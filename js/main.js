0bcc1161e1b10daee1e3e906ae897a94e61f3320
dudusilvacosta <dudu_silvacosta@hotmail.com>
2023-10-28 01:09:02 -0300

    bacgrouwnd header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+window.addEventListener('scroll', function() {
+    let header = document.getElementById('header');
+    
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
 let nav = document.getElementById('nav')
 
 function toggleMenu() {
